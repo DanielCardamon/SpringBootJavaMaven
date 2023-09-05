6552076de9e36d805f5a0e19ea2ed43eac459b6b
DanielCardamon <112758759+DanielCardamon@users.noreply.github.com>
2023-09-05 11:17:59 -0300

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ConsultaCatalogo from "./components/ConsultaCatalogo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>FATEC ZL</h1>
+      <ConsultaCatalogo />
+    </div>
+  );
+}
